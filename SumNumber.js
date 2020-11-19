c4c773db0cb0b3a4a5f1a174f6a6d2547ce10083
abdaliDahir <abdali.dahir@gmail.com>
2020-11-19 12:49:15 +0100

    update Sum function

--- SumNumber.js
+++ SumNumber.js
@@ -1,5 +1,8 @@
-const SumNumber = (num1,num2) => {
-  return num1 + (num2 || null);
-}
+// const SumNumber = (num1,num2) => {
+//   return num1 + (num2 || null);
+// }
 
+const SumNumber = (...Numbers) => {
+  return Numbers.reduce((prev, curr) => prev + curr, 0);
+}
 module.exports = SumNumber;
--- __tests__/sumNumber.test.js
+++ __tests__/sumNumber.test.js
@@ -1,10 +1,18 @@
 const SumNumber = require('../SumNumber');
 
+test('should return 0 if there is o argm', () => {
+  expect(SumNumber()).toBe(0);
+})
+
 test('should return', () => {
   expect(SumNumber(10,20)).toBe(30);
 })
 
 test('should return Num', () => {
   expect(SumNumber(10)).toBe(10);
+})
+
+test('should return sum of numbers', () => {
+  expect(SumNumber(1,20,5,9,7)).toBe(42)
 })
 
